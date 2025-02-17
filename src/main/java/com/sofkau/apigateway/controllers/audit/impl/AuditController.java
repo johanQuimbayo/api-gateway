b0b5317aaf801d1adb35b9fc2c7360ecd4a16810
johan.quimbayo <johan.quimbayo@sofka.com.co>
2025-02-17 12:20:20 -0500

    corrections

--- src/main/java/com/sofkau/apigateway/controllers/audit/impl/AuditController.java
+++ src/main/java/com/sofkau/apigateway/controllers/audit/impl/AuditController.java
@@ -7,11 +7,8 @@ import com.sofkau.apigateway.models.audit.response.PageResponseDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import org.springframework.data.domain.Pageable;
@@ -26,12 +23,12 @@ public class AuditController implements IAuditController {
     private IAuditClient auditClient;
 
     @GetMapping("/byIdEntidad/{idEntidad}")
-    public Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(@PathVariable String idEntidad, Pageable pageable, ServerHttpRequest originalRequest) {
+    public Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(@PathVariable String idEntidad, @RequestParam Pageable pageable, ServerHttpRequest originalRequest) {
         return auditClient.getMessagesByIdEntidad(idEntidad, pageable, originalRequest);
     }
 
     @GetMapping("/byRecurso/{recurso}")
-    public Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(@PathVariable String recurso, Pageable pageable, ServerHttpRequest originalRequest) {
+    public Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(@PathVariable String recurso, @RequestParam Pageable pageable, ServerHttpRequest originalRequest) {
         return auditClient.getMessagesByRecurso(recurso, pageable, originalRequest );
     }
 
