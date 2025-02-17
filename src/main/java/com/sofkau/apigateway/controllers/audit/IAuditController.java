f5efb6ac5c5bdae0fd84324d7472427a70e3863f
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-02-17 12:37:40 -0500

    se agregan cambios a la consulta paginada

--- src/main/java/com/sofkau/apigateway/controllers/audit/IAuditController.java
+++ src/main/java/com/sofkau/apigateway/controllers/audit/IAuditController.java
@@ -3,12 +3,23 @@ package com.sofkau.apigateway.controllers.audit;
 import com.sofkau.apigateway.models.audit.response.MessageDTO;
 import com.sofkau.apigateway.models.audit.response.PageResponseDTO;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
 import org.springframework.data.domain.Pageable;
 
 
 public interface IAuditController {
-    Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(String idEntidad, Pageable pageable, ServerHttpRequest originalRequest);
-    Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(String recurso, Pageable pageable, ServerHttpRequest originalRequest);
+    Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(
+            String idEntidad,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            ServerHttpRequest originalRequest);
+
+    Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(
+            String recurso,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            ServerHttpRequest originalRequest);
 }
+
--- src/main/java/com/sofkau/apigateway/controllers/audit/impl/AuditController.java
+++ src/main/java/com/sofkau/apigateway/controllers/audit/impl/AuditController.java
@@ -6,6 +6,7 @@ import com.sofkau.apigateway.models.audit.response.MessageDTO;
 import com.sofkau.apigateway.models.audit.response.PageResponseDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,13 +24,25 @@ public class AuditController implements IAuditController {
     private IAuditClient auditClient;
 
     @GetMapping("/byIdEntidad/{idEntidad}")
-    public Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(@PathVariable String idEntidad, @RequestParam Pageable pageable, ServerHttpRequest originalRequest) {
+    public Mono<PageResponseDTO<MessageDTO>> getMessagesByIdEntidad(
+            @PathVariable String idEntidad,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            ServerHttpRequest originalRequest) {
+
+        Pageable pageable = PageRequest.of(page, size);
         return auditClient.getMessagesByIdEntidad(idEntidad, pageable, originalRequest);
     }
 
     @GetMapping("/byRecurso/{recurso}")
-    public Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(@PathVariable String recurso, @RequestParam Pageable pageable, ServerHttpRequest originalRequest) {
-        return auditClient.getMessagesByRecurso(recurso, pageable, originalRequest );
+    public Mono<PageResponseDTO<MessageDTO>> getMessagesByRecurso(
+            @PathVariable String recurso,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            ServerHttpRequest originalRequest) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return auditClient.getMessagesByRecurso(recurso, pageable, originalRequest);
     }
 
 }
